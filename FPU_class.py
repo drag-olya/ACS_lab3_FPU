992be06d808d7fe04e2cdf3991f77d536e2aa124
Olha Drahomeretska <drahomeretskak13@gmail.com>
2022-11-30 19:37:12 +0200

    add commands

--- FPU_class.py
+++ FPU_class.py
@@ -1,7 +1,8 @@
-from math import inf, nan
+from math import inf, nan, pi
 
 
-class Coprocessor:
+class FPU:
+
     P = 12  # bits for characteristic
     M = 9  # bits for mantissa
 
@@ -9,9 +10,7 @@ class Coprocessor:
         self.stack = []
         for _ in range(8):
             self.stack.append(['0 '] + ['0']*self.P + [' 0 '] + ['0']*self.M)
-
         self._dec_stack = []
-
         self.BIAS = 2 ** (self.P - 1) - 1
         self.max = ['0', ' '] + ['1'] * (self.P - 1) + ['0'] + [' 1 '] + ['1'] * self.M
         self.min = ['1', ' '] + ['1'] * (self.P - 1) + ['0'] + [' 1 '] + ['0'] * self.M
@@ -33,10 +32,6 @@ class Coprocessor:
     def _dec2bin(x, bits) -> list:
         bin_str = format(x, f'0{bits}b')
         return list(bin_str)
-
-    @staticmethod
-    def _bin2dec(x: list) -> int:
-        return int(''.join(x), 2)
 
     @staticmethod
     def _float2bin(x, bits) -> list:
@@ -64,7 +59,6 @@ class Coprocessor:
         while x >= 2:
             q += 1
             x /= 2
-        print(f'x  {x}')
         char = self.BIAS + q
         return sign + [' '] + self._dec2bin(char, self.P) + [' 1 '] + self._float2bin(x, self.M)
 
@@ -72,38 +66,42 @@ class Coprocessor:
         print(f'IR: {com}')
         for i in range(1, 9):
             print(f'R{i}: {self._list2str(self.stack[i-1])}')
-        print(f'PS: {self.PS}')
+        print(f'\nPS: {self.PS}')
         print(f'PC: {self.PC}')
         print(f'TC: {self.TC}')
         print('**********************************')
 
     def do_command(self, com: str):
-        com = com.split()
-
         self.TC += 1
         self.PC += 1
         self._print_stat(com)
 
-        try:
-            if com[0] == 'mov':
-                self.mov(float(com[1]))
-            elif com[0] == 'add':
-                self.add()
-            elif com[0] == 'mult':
-                self.mult()
-            elif com[0] == 'div':
-                self.div()
-            else:
-                raise Exception('Inappropriate command')
+        com_list = com.split()
 
-            self.TC += 1
-            self._print_stat(com)
-            self.TC = 0
+        if com_list[0] == 'mov':
+            self.mov(com_list[1])
+        elif com_list[0] == 'add':
+            self.add()
+        elif com_list[0] == 'mult':
+            self.mult()
+        elif com_list[0] == 'div':
+            self.div()
 
-        except Exception as err:
-            print(str(err))
+        self.TC += 1
+        self._print_stat(com)
+        self.TC = 0
+
+    def _refresh_stack(self, x):
+        self._dec_stack.append(x)
+        x_ieee74 = self.dec2ieee74(x)
+        self.stack[0] = x_ieee74
+        for i in range(1, 7):
+            self.stack[i] = self.stack[i + 1]
+            self.stack[7] = self.num1_0E0
+        self.PS = x_ieee74[0]
 
     def mov(self, x):
+        x = pi if x == 'pi' else float(x)
         self._dec_stack.append(x)
         for i in range(7, -1, -1):
             self.stack[i] = self.stack[i-1]
@@ -111,19 +109,9 @@ class Coprocessor:
         self.stack[0] = x_ieee74
         self.PS = x_ieee74[0]
 
-    def _refresh_stack(self, x):
-        self._dec_stack.append(x)
-        x_ieee74 = self.dec2ieee74(x)
-        self.stack[0] = x_ieee74
-        for i in range(1, 8):
-            self.stack[i] = self.stack[i + 1]
-        self.PS = x_ieee74[0]
-        # check stack
-
     def div(self):
         x = self._dec_stack.pop()
         y = self._dec_stack.pop()
-        # check stack
         if y == 0:
             if x > 0:
                 self.stack[0] = self.inf
@@ -143,21 +131,9 @@ class Coprocessor:
         y = self._dec_stack.pop()
         res = x + y
         self._refresh_stack(res)
-        # check stack
 
     def mult(self):
         x = self._dec_stack.pop()
         y = self._dec_stack.pop()
         res = x * y
         self._refresh_stack(res)
-
-
-copr = Coprocessor()
-copr.do_command('mov 3')
-copr.do_command('mov 4')
-"""
-print(copr._list2str(copr.stack[2]))
-print(copr._list2str(copr.min))
-print(copr._list2str(copr.dec2ieee74(0.008)))
-print(copr._list2str(copr.dec2ieee74(-7.3)))
-print(copr._list2str(copr.dec2ieee74(2.31)))"""
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from Coprocessor_class import Coprocessor
+# Simulation model of a mathematical processor
+# Variant 12.9.1
+# 12 bits for characteristic
+# 9 bits for mantissa
+# formula #1: 3/4*x + x/y + pi
+
+from FPU_class import FPU
 
 
 def list2str(l):
@@ -7,22 +13,36 @@ def list2str(l):
 
 def main():
 
-    c = Coprocessor()
+    fpu = FPU()
 
     print('Done by Olha Drahomeretska\nVariant 12.9.1\n')
-    print(list2str(['s '] + ['p']*c.P + [' n '] + ['m']*c.M) + ': number in IEEE74 format')
-    print(f'{list2str(c.max)}: maximum positive ')
-    print(f'{list2str(c.min)}: minimum negative')
-    print(f'{list2str(c.min_non_zero)}: minimum positive')
-    print(f'{list2str(c.num1_0E0)}: number + 1.0E0')
-    print(f'{list2str(c.inf)}: +inf')
-    print(f'{list2str(c._inf)}: -inf')
-    print(f'{list2str(c.dec2ieee74(0.008))}: non-normilized (0.008)')
-    print(f'{list2str(c.NaN)}: NaN value\n')
+    print(list2str(['s '] + ['p']*fpu.P + [' n '] + ['m']*fpu.M) + ': number in IEEE74 format')
+    print(f'{list2str(fpu.max)}: maximum positive (2 - 2 ^(-{fpu.M})) * 2 ^ {fpu.BIAS}')
+    print(f'{list2str(fpu.min)}: minimum negative -(2 - 2 ^(-{fpu.M})) * 2 ^ {fpu.BIAS}')
+    print(f'{list2str(fpu.min_non_zero)}: minimum positive 1 * 2 ^ -({fpu.M + fpu.BIAS - 1})')
+    print(f'{list2str(fpu.num1_0E0)}: number + 1.0E0')
+    print(f'{list2str(fpu.inf)}: +inf')
+    print(f'{list2str(fpu._inf)}: -inf')
+    print(f'{list2str(fpu.dec2ieee74(0.008))}: non-normilized (0.008)')
+    print(f'{list2str(fpu.NaN)}: NaN value\n')
 
-    print('formula to calculate: 3/4*x + x/y + pi')
+    print('formula to calculate: 3/4*x + x/y + pi\n')
 
+    x = input('Enter x: ')
+    y = input('Enter y: ')
+    print()
 
+    fpu.do_command('mov pi')
+    fpu.do_command(f'mov {y}')
+    fpu.do_command(f'mov {x}')
+    fpu.do_command('div')
+    fpu.do_command('add')
+    fpu.do_command(f'mov {x}')
+    fpu.do_command('mov 4')
+    fpu.do_command('mov 3')
+    fpu.do_command('div')
+    fpu.do_command('mult')
+    fpu.do_command('add')
 
 
 main()
